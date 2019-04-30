594ccdf1d2b14f47607acb398892834bbb07465d
Toreole <toreole.official@gmail.com>
2019-04-30 12:50:52 +0200

    yes

---
+++ Assets/_Scripts/ToggleInteract.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HighHouse {
+    public class ToggleInteract : MonoBehaviour, IInteractable
+    {
+        [SerializeField]
+        protected Animator anim;
+
+        public void Interact(PlayerController player)
+        {
+            anim.SetTrigger("Toggle");
+        }
+    }
+}
